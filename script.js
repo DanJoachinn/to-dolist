cc457318ba16c03701acee9a935291bea2baaba2
Daniel Ortiz Joachin <31485342+DanJoachinn@users.noreply.github.com>
2018-05-13 21:29:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: "#appVue",
+  data: {
+    lists: [],
+    newKeep: ""
+
+  },
+
+  methods: {
+    addKeep: function(){
+      this.lists.push({keep: this.newKeep, completed: false});
+
+      this.newKeep = "";
+    },
+    remover: function(){
+      this.lists.pop();
+    },
+ }
+});
