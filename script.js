99ea5aae46dbfb22ea964f95e3cff0ea5b529e2c
Daniel Ortiz Joachin <31485342+DanJoachinn@users.noreply.github.com>
2018-05-14 02:07:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+
 new Vue({
+
   el: "#appVue",
   data: {
-    lists: [],
+    lists: [
+    ],
     newKeep: ""
 
   },
 
   methods: {
     addKeep: function(){
-      this.lists.push({keep: this.newKeep, completed: false});
 
+      this.lists.push({keep: this.newKeep, completed: false});
       this.newKeep = "";
+
+      var  firebaseRef = firebase.database().ref();
+      var input = document.getElementById("input");
+      var txt = input.value;
+
+      firebaseRef.child("Tareas").push().set(txt);
+
     },
     remover: function(){
       this.lists.pop();
+
     },
-    submit: function(){
-
-      window.alert("working");
-
+    datos: function(){
+      var rootRef = firebase.database().ref().child("Tareas");
     }
-
  }
 });
